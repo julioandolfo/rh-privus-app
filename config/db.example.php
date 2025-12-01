e1cbcb81024942509d9e7c5819839c8b0d361ace
Julio Andolfo <juliosertori@gmail.com>
2025-12-01 15:39:07 -0300

    correcoes23

---
+++ config/db.example.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Exemplo de Configuração de Banco de Dados
+ * 
+ * INSTRUÇÕES:
+ * 1. Copie este arquivo para config/db.php
+ * 2. Preencha com as credenciais corretas do seu ambiente
+ * 3. O arquivo config/db.php está no .gitignore e não será versionado
+ */
+
+return [
+    'host' => 'localhost',           // Host do banco de dados (ex: localhost, poa.layerserver.com)
+    'dbname' => 'rh-privus',        // Nome do banco de dados
+    'username' => 'root',            // Usuário do banco
+    'password' => '',                // Senha do banco
+];
+
