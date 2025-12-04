bf61f5f4d06a798aa3357a6370bc923e2f88122f
Julio Andolfo <juliosertori@gmail.com>
2025-12-04 12:04:41 -0300

    correcoes50

--- includes/upload_documento.php
+++ includes/upload_documento.php
@@ -109,18 +109,3 @@ function is_image_file($filepath) {
     return in_array($extension, $image_extensions);
 }
 
-/**
- * Formata tamanho do arquivo para exibição
- */
-function format_file_size($bytes) {
-    if ($bytes >= 1073741824) {
-        return number_format($bytes / 1073741824, 2) . ' GB';
-    } elseif ($bytes >= 1048576) {
-        return number_format($bytes / 1048576, 2) . ' MB';
-    } elseif ($bytes >= 1024) {
-        return number_format($bytes / 1024, 2) . ' KB';
-    } else {
-        return $bytes . ' bytes';
-    }
-}
-
